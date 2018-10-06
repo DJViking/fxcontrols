00166174592e518eab7a9f43c7cde43c19dee442
Sverre Moe <sverre@spacetec.no>
2018-10-06 23:25:01 +0200

    Remove third party javafx dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,14 @@
-/**
- * FXControls
- */
+/** FXControls */
 module no.smeaworks.fxcontrols {
 
+    /* JavaFX modules */
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
     requires javafx.graphics;
     requires javafx.media;
 
-    /* Named modules */
-    requires jfxtras.controls;
-    requires com.jfoenix;
-
-    /* Automatic modules */
-    requires controlsfx;
-
+    /* Log4j modules */
     requires org.apache.logging.log4j;
 
     exports no.smeaworks.fxcontrols to javafx.graphics, javafx.fxml;
